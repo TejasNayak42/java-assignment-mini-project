d8537216e817ba71e5f1068e512d256d3fea936e
srajankumar <kumarsrajan02@gmail.com>
2023-02-13 20:34:40 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// app.get("/", function (req, res) {
-//   res.send("Hello World!");
-// });
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
 
-// app.get("/user", function (req, res) {
-//   res.send("Hello World from user");
-// });
+app.get("/user", function (req, res) {
+  res.send("Hello World from user");
+});
 
-// app.get("/about", function (req, res) {
-//   res.sendFile(__dirname + "/about.html");
-// });
+app.get("/about", function (req, res) {
+  res.sendFile(__dirname + "/about.html");
+});
 
-// app.get("/hungry", function (req, res) {
-//   res.sendFile(__dirname + "/hungry.html");
-// });
+app.get("/hungry", function (req, res) {
+  res.sendFile(__dirname + "/hungry.html");
+});
 
-// app.listen(3000, function () {
-//   console.log("Server is up and running...");
-// });
-var java = require("java");
-var javaLangSystem = java.import("java.lang.System");
-
-javaLangSystem.out.printlnSync("I love gfg!");
+app.listen(3000, function () {
+  console.log("Server is up and running...");
+});
