61348bfd1bed9650cf607955c0137fd6ff6d7833
srajankumar <kumarsrajan02@gmail.com>
2023-02-13 20:28:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("Hello World!");
+// });
+
+// app.get("/user", function (req, res) {
+//   res.send("Hello World from user");
+// });
+
+// app.get("/about", function (req, res) {
+//   res.sendFile(__dirname + "/about.html");
+// });
+
+// app.get("/hungry", function (req, res) {
+//   res.sendFile(__dirname + "/hungry.html");
+// });
+
+// app.listen(3000, function () {
+//   console.log("Server is up and running...");
+// });
+var java = require("java");
+var javaLangSystem = java.import("java.lang.System");
+
+javaLangSystem.out.printlnSync("I love gfg!");
